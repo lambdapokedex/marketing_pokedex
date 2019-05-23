4e71d592bf8ae11b2a75468acf3fc2f77806b069
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-05-22 21:47:59 -0400

    Array and components added to Features page

--- JS/features.js
+++ JS/features.js
@@ -1,17 +1,64 @@
-
 class Card {
-    constructor(card) {
-        this.card = card;
-        this.card.addEventListener('click', () => this.flip());
-    }
-
-    flip() {
-        this.card.classList.toggle('flipped');
-    }
+  constructor(card) {
+    this.card = card;
+    console.log(this.card);
+    //this.card.addEventListener("click", () => this.flip());
+    this.infoNum = document.querySelector(
+      `.card[data-tab='${this.card.dataset.tab}']`
+    );
+    this.front = document.querySelector(
+      `.cardFront[data-tab='${this.card.dataset.tab}']`
+    );
+    this.back = document.querySelector(
+        `.cardBack[data-tab='${this.card.dataset.tab}']`
+      );
+    console.log(this.front);
+    this.back.addEventListener("click", () => this.select());
+    this.cardFront = new CardFront(this.front);
+  }
+  
+  select() {
+    const backs = document.querySelectorAll(".cardBack");
+    const fronts = document.querySelectorAll(".cardFront");
+    Array.from(backs).forEach(function(card) {
+      card.classList.remove("cardBack-flipped");
+    });
+    Array.from(fronts).forEach(function(front) {
+        front.classList.remove("cardFront-flipped");
+      });
+    this.cardFront.select();
+    this.back.classList.add('cardBack-flipped');
+  }
 }
 
-const cards = document.querySelectorAll('.card');
+class CardFront {
+  constructor(front) {
+    this.front = front;
+    this.front.addEventListener('click', () => this.flip());
+    this.back = document.querySelector(
+        `.cardBack[data-tab='${this.front.dataset.tab}']`
+      );
+  }
+
+  select() {
+    const fronts = document.querySelectorAll(".cardFront");
+    const backs = document.querySelectorAll('.cardBack')
+    Array.from(fronts).forEach(function(front) {
+      front.classList.remove("cardFront-flipped");
+    });
+    Array.from(backs).forEach(function(card) {
+        card.classList.remove("cardBack-flipped");
+      });
+    this.front.classList.add("cardFront-flipped");
+  }
+  flip() {
+  this.front.classList.remove('cardFront-flipped');
+  this.back.classList.remove('cardBack-flipped');
+  }
+}
+
+const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
-    return new Card(card);
+  return new Card(card);
 });
